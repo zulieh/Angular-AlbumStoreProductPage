991b49bbc34f90e88dc7cf5baad84afc7dcbe144
swaleeha <tzuleiha@gmail.com>
2020-10-08 18:02:39 +0100

    new

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -21,7 +21,8 @@ export class ProductService {
   }
 
   getProducts(): Observable<Product[]> {
-    return this._http.get(this._productsUrl).map (response => <Product[]>response.json());
+    return this._http.get(this._productsUrl).map (response => 
+    <Product[]>response.json());
    }
 
   
